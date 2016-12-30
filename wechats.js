67197ce1f896e2f3e57f11847e4ed4cc1534748c
zhangxuchao <2212362272@qq.com>
2016-12-30 17:55:01 +0800

    Create wechats.js

---
+++ wechats.js
@@ -0,0 +1,5 @@
+1. 网络使用了代理
+
+Failed to load resource: net::ERR_NAME_NOT_RESOLVED http://1709827360.appservice.open.weixin.qq.com/appservice
+问题原因：通常是由于系统设置了代理如Shadowsocks等。
+解决方案：关闭代理，或者依次点击工具栏“动作”-"设置"，选择“不使用任何代理，勾选后直连网络”。
